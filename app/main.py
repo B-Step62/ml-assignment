5c1ae21a6f85106b56d480b0d7704d017da257ac
B-Step62 <bsatbeyp@gmail.com>
2023-06-18 01:02:36 +0900

    Bare-bones app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,41 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from translator.M2M100 import M2M100Translator, M2M100ModelType
+
 
 app = FastAPI()
+translator = M2M100Translator(M2M100ModelType.M2M100_418M)
 
 
+class TranslationRecord(BaseModel):
+    id: str
+    text: str
+    
+        
+class TranslationPayload(BaseModel):
+    records: List[TranslationRecord]
+    fromLang: str
+    toLang: str
+
+
+class TranslationRequest(BaseModel):
+    payload: TranslationPayload
 
 
 @app.post("/translation")
-def translation(text: str,source_language: str,  target_language: str):
-    return {"result": "人生はチョコレートの箱のようなものだ。"}
+async def translation(request: TranslationRequest):
+    payload = request.payload
+    
+    results = []
+    for record in payload.records:
+        translated = translator.translate(record.text, payload.fromLang, payload.toLang)
+        results.append({"id": record.id, "text": translated})   
+        
+    return {"result": results}
+
+
+# For test
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=9527)
